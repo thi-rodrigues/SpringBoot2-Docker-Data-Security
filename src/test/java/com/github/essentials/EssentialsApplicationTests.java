1fccc97ea05f0248325f0b2ae792f8c9e98318a0
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-15 17:24:11 -0300

    Project Created

---
+++ src/test/java/com/github/essentials/EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
